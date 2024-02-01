fbf1eb364854b76d96f1b99ce114a73f3f11937a
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-02-01 16:06:44 +0000

    connect-four question info added

--- 4kyu/connect-four.js
+++ 4kyu/connect-four.js
@@ -1,34 +1,28 @@
-piecesPositionList = [
-  "B_Red",
-  "D_Yellow",
-  "G_Red",
-  "D_Yellow",
-  "E_Red",
-  "F_Yellow",
-  "C_Red",
-  "G_Yellow",
-  "A_Red",
-  "A_Yellow",
-  "D_Red",
-  "E_Yellow",
-  "A_Red",
-  "A_Yellow",
-  "D_Red",
-  "A_Yellow",
-  "B_Red",
-  "D_Yellow",
-  "G_Red",
-  "C_Yellow",
-  "E_Red",
-  "G_Yellow",
-  "B_Red",
-  "B_Yellow",
-  "G_Red",
-  "C_Yellow",
-  "C_Red",
-  "E_Yellow",
-  "D_Red",
-];
+// Connect Four
+
+// Take a look at wiki description of Connect Four game:
+
+// Wiki Connect Four
+
+// The grid is 6 row by 7 columns, those being named from A to G.
+
+// You will receive a list of strings showing the order of the pieces which dropped in columns:
+
+//   piecesPositionList = ["A_Red",
+//                         "B_Yellow",
+//                         "A_Red",
+//                         "B_Yellow",
+//                         "A_Red",
+//                         "B_Yellow",
+//                         "G_Red",
+//                         "B_Yellow"]
+// The list may contain up to 42 moves and shows the order the players are playing.
+
+// The first player who connects four items of the same color is the winner.
+
+// You should return "Yellow", "Red" or "Draw" accordingly.
+
+/* Solution */
 
 function whoIsWinner(piecesPositionList) {
   // Game board
---
+++ 7kyu/array-squareup-b.js
@@ -0,0 +1,13 @@
+function squareUp(n) {
+
+    for (let i=n; i > 0; i++) {
+        let currNum = 0 
+        for (let j= )
+        
+    }
+
+
+  return []; // Do your magic!
+}
+
+squareUp(3);
