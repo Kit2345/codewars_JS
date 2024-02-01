2f67a9c1055996d0fe294595420db5af3a86c8f2
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-02-01 16:35:40 +0000

    cats and shelves 7kyu initiated

--- title.js
+++ title.js
@@ -2,6 +2,6 @@ function fileName(name) {
   console.log(name.replaceAll(" ", "-"));
 }
 
-let name = "Determine offspring sex based on genes XX and XY chromosomes";
+let name = "Cats and shelves";
 
 fileName(name);
